a93dbe34348d94c1715f11df820873b4e0bc6028
DanTel <DaniyarT2005@gmail.com>
2025-04-28 23:34:35 +0500

    feat: add solution for streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import { createReadStream } from "node:fs";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
 const read = async () => {
-    // Write your code here 
+  // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const filepath = join(__dirname, "files", "fileToRead.txt");
+
+  const rs = createReadStream(filepath);
+
+  rs.pipe(process.stdout);
 };
 
 await read();
